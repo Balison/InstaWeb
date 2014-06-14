d518f0d1979e47de87353850df680bcb7e9795a7
Alison Fernandez Blanco <fernablanco.alison@gmail.com>
2014-06-14 15:36:21 -0400

    Diego te adoro pero me estoy haciendo a las dificiles, gracias por los doritos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instagram::Application.config.secret_key_base = '18415600a05572586545e4240290c499e922793e002a95fa3d4f07773a3e7a1af5fa133bf9e2b08f2c32fa91ae1e3f7c400825abf242f4e6b03acaab4df0567d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Instagram::Application.config.session_store :cookie_store, key: '_Instagram_session'
