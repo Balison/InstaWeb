cb4230d4764eb6149417159609b8f3139bf84f96
diego-gabriel <diego.gabriel.nd@gmail.com>
2014-06-21 18:43:38 -0400

    registro

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+	def new	
+	end
+	def index
+		@users = User.all
+	end
+	def create
+		user = User.new(user_params)
+		user.save
+		redirect_to users_path
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end	
+
+	def destroy
+		user = User.find(params[:id])
+		user.destroy
+
+		redirect_to users_path
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :password)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Instagram::Application.routes.draw do
+
+  resources :users do
+  end
+
   get "/" => "instagram#index"
   root "instagram#index"
 end
