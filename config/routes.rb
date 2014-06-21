86ff21b261b21879145e0c464d06f0b31ad1090c
Alison Fernandez Blanco <fernablanco.alison@gmail.com>
2014-06-21 18:48:48 -0400

    mm

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Instagram::Application.routes.draw do
+
+  resources :users do
+  end
+
   get "/" => "instagram#index"
+  match '/login', to: 'users#login', via: "post"
   root "instagram#index"
 end
