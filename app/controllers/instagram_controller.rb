78fa136a06ceecdd579849980a3b62df38da288f
diego-gabriel <diego.gabriel.nd@gmail.com>
2014-06-25 15:55:41 -0400

    sessions

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,4 +1,21 @@
 class InstagramController < ApplicationController
   def index
+  	@user = User.find_by name: params[:username]
+  	@params = params
+  	if (@user)
+	  	if @user.name == params[:password]
+	  		session[:user_id] = @user.name
+	  	end
+	end
+
+  	if session[:user_id]
+  		@user = User.find_by name: session[:user_id]
+  		render "userIndex"
+  	end
+  end
+
+  def closeSession
+  	session[:user_id] = nil
+  	redirect_to controller: "instagram", action: "index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Instagram::Application.routes.draw do
   end
 
   get "/" => "instagram#index"
+  get "/closeSession" => "instagram#closeSession"
+  match "/" => "instagram#index", via: "post"
   match '/login', to: 'users#login', via: "post"
   root "instagram#index"
 end
